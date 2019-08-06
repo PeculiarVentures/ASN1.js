074d9726278ab74ed669a98c4091c003dd45b219
Tom Hirst <tom.hirst@ipe-technologies.com>
2019-08-06 09:44:47 +0100

    Handle 0 length buffer in LocalFromBER function
    This internal function was assigning the error string to this.error,
    rather than returnObject.error, causing an exception.

--- src/asn1.js
+++ src/asn1.js
@@ -5301,7 +5301,7 @@ function LocalFromBER(inputBuffer, inputOffset, inputLength)
 	//region Initial checks
 	if(intBuffer.length === 0)
 	{
-		this.error = "Zero buffer length";
+		returnObject.error = "Zero buffer length";
 		return {
 			offset: (-1),
 			result: returnObject
